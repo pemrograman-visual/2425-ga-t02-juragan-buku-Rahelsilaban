19f709e87dcb7c4541b469b5b95fa4d23f3b1535
Rahelsilaban <rahelj.e.silaban23@gmail.com>
2024-09-27 11:14:08 +0700

    kami sudah menyelesaikan tugas

--- T02.java
+++ T02.java
@@ -4,7 +4,10 @@
 import java.util.*;
 import java.lang.Math;
 
-public class T02{
+import java.util.*;
+import java.lang.Math;
+
+public class T02 {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
